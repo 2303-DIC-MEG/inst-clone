434e164fad9e44a98eac53ddda3fcdf28ca0c835
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-04-08 13:30:28 +0900

    セキュリティ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validetion { email.downcase! }
+  has_secure_password
+  validates :password, length: { minmum: 6 }
 end
