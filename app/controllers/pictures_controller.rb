32a7a4b673b5231d8cffc0be861e3100401f5ec9
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-04-11 09:09:35 +0900

    編集画面作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only:[:show, :edit, :update
+]
   def index
     @pictures = Picture.all
   end
@@ -17,10 +19,17 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -30,5 +39,9 @@ class PicturesController < ApplicationController
 
   def picture_params
     params.require(:picture).permit(:image, :content)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
