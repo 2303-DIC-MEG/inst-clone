2941eee13e046fea3a45cf73f5673186a9d624f2
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-04-17 13:07:43 +0900

    50件分のデータ登録

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,12 @@
 class FavoritesController < ApplicationController
-  before_action :set_picture
-  before_action :authenticate_user!   # ログイン中のユーザーのみに許可（未ログインなら、ログイン画面へ移動）
-
-  # お気に入り登録
   def create
-    if @picture.user_id != current_user.id   # 投稿者本人以外に限定
-      @favorite = Favorite.create(user_id: current_user.id, picture_id: @picture.id)
-    end
-  end
-  # お気に入り削除
-  def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, picture_id: @picture.id)
-    @favorite.destroy
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの画像をお気に入り登録しました"
   end
 
-  private
-  def set_picture
-    @picture = Picture.find(params[:post_id])
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんのブ画像をお気に入り解除しました"
   end
 end
+
